7c76592834e39d0823f8ef6a0c2a191e0f41ce79
DanielPedraza509 <pedrazajaca2001@gmail.com>
2023-12-09 19:24:32 -0500

    Modificacion de etiquetas y codigo base para convertir texto a audio

---
+++ TextToAudio.py
@@ -0,0 +1,10 @@
+import pyttsx3
+
+def reproducir_audio(palabra):
+    engine = pyttsx3.init()
+    engine.say(palabra)
+    engine.runAndWait()
+
+
+detector = "HOLA"
+reproducir_audio("La palabra deletreada fue: " +str(detector))
